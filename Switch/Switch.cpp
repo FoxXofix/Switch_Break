8d9d3af2c42265238edb6c3d34e2502c4e5dfbbe
Александр Важнёв <vazhnevalex@gmail.com>
2025-02-18 19:12:12 +0300

    Добавьте файлы проекта.

---
+++ Switch/Switch.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x;
+	cin >> x;
+	switch (x)
+	{
+	case 1:
+	case 2:
+	case 3:
+	case 4:
+	case 5: 
+		cout << "A"; 
+		break;
+	case 6:
+	case 7: 
+		cout << "B"; 
+		break;
+	default: 
+		cout << "ABOBA";
+	}
+	return 0;
+}
