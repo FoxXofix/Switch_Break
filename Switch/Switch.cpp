b1d7dbec46fb4f010c9c9ca1748d1f5481c8e6ce
FoxXofix <vazhnevalex@gmail.com>
2025-04-02 15:52:34 +0300

    Update Switch.cpp
    normal syntax

--- Switch/Switch.cpp
+++ Switch/Switch.cpp
@@ -1,25 +1,36 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
 	int x;
+	cout << "choose from 1 to 7\n" ;
 	cin >> x;
 	switch (x)
 	{
 	case 1:
+		cout << "A";
+		break;
 	case 2:
+		cout << "B";
+		break;
 	case 3:
+		cout << "C";
+		break;
 	case 4:
-	case 5: 
-		cout << "A"; 
+		cout << "D";
+		break;
+	case 5:
+		cout << "E";
 		break;
 	case 6:
-	case 7: 
-		cout << "B"; 
+		cout << "F";
 		break;
-	default: 
-		cout << "ABOBA";
+	case 7:
+		cout << "G";
+		break;
+	default:
+		cout << "you put wrong meaning";
 	}
 	return 0;
 }
